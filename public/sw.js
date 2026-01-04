a20e9c67f158865d9da4009e5a85d43bb67ac848
hariomp11 <gameversenexus@gmail.com>
2026-01-04 13:29:12 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,14 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", () => {
+  console.log("Service Worker Activated");
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
 
